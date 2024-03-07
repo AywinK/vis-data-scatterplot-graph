178054bbf7fc167d1eb47784ae15890f78e62f2a
Aywin <AywinK@hotmail.com>
2024-03-07 16:05:38 +0000

    16/16 passing

--- script.js
+++ script.js
@@ -46,7 +46,6 @@ function renderChart() {
 
     // add circle for each data point
     svg.append("g")
-        .attr("fill", "steelblue")
         .selectAll("circle")
         .data(data)
         .join("circle")
@@ -56,7 +55,61 @@ function renderChart() {
         .attr("r", 5)
         .attr("data-xvalue", d => d.Year)
         .attr("data-yvalue", d => (new Date(d.Seconds * 1000)))
+        .style("fill", d => d?.Doping !== "" ? "red" : "steelblue")
+
+    // add legend to chart
+    const legend = svg.append("g")
+        .attr("id", "legend")
+        .attr("transform", `translate(${width - 2 * margin}, ${2 * margin})`);
+
+    const legendItems = legend.selectAll("g")
+        .data(["Doping", "No Doping"])
+        .join("g")
+        .attr("transform", (d, i) => `translate(0, ${i * margin})`);
+
+    legendItems.append("rect")
+        .attr("x", 0)
+        .attr("y", 0)
+        .attr("width", 10)
+        .attr("height", 10)
+        .attr("fill", d => d === "Doping" ? "red" : "steelblue");
+
+    legendItems.append("text")
+        .attr("x", 15)
+        .attr("y", 10)
+        .attr("dy", "0em")
+        .text(d => d);
+
+    // add tooltips to dots
+    svg.selectAll(".dot")
+        .on("mouseenter", handleMouseEnter)
+        .on("mouseout", handleMouseOut);
 
     // append svg chart to root
     document.getElementById("root").append(svg.node());
+}
+
+const handleMouseEnter = (e, d) => {
+    const x = e.pageX;
+    const y = e.pageY;
+    console.log(x, y)
+
+    const divEl = document.createElement("div");
+    divEl.setAttribute("id", "tooltip");
+    divEl.setAttribute("data-year", d.Year);
+    divEl.style.top = (y + 10) + "px";
+    divEl.style.left = (x + 10) + "px";
+    const tooltipHtml = `
+    Name: ${d.Name} <br />
+    Nationality: ${d.Nationality} <br />
+    Year: ${d.Year} <br />
+    Time: ${d.Time} <br />
+    ${d.Doping !== "" ? d.Doping : ""}
+    `
+    divEl.innerHTML = tooltipHtml
+    document.getElementById("root").appendChild(divEl);
+}
+
+const handleMouseOut = () => {
+    document.getElementById("tooltip").remove();
 }
