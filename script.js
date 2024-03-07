893a997f8f0d4bbab5016751572a1da30ee37a94
Aywin <AywinK@hotmail.com>
2024-03-07 15:35:42 +0000

    13/16

--- script.js
+++ script.js
@@ -19,48 +19,35 @@ function renderChart() {
 
     // x scale
     const xScale = d3.scaleLinear()
-        .domain([d3.min(data, data => data.Year - 1), d3.max(data, data => data.Year + 1)])
+        .domain(d3.extent(data, d => d.Year)).nice()
         .range([margin, width - margin]);
-    console.log(xScale(2015))
 
     // y scale
     const yScale = d3.scaleTime()
-        .domain([d3.max(data, data => new Date(data.Seconds * 1000)), d3.min(data, data => new Date(data.Seconds * 1000))])
-        .range([height - margin, margin]);
+        .domain(d3.extent(data, d => (new Date(d.Seconds * 1000)))).nice()
+        .range([margin, height - margin]);
 
     // create svg chart
     const svg = d3.create("svg")
         .attr("width", width)
         .attr("height", height);
 
-    // add x-axis
-    svg.append("g")
-        .attr("transform", `translate(0,${height - margin})`)
-        .attr("id", "x-axis")
-        .call(d3.axisBottom(xScale));
-
-    // add y-axis
-    svg.append("g")
-        .attr("transform", `translate(${margin}, 0)`)
-        .attr("id", "y-axis")
-        .call(d3.axisLeft(yScale));
-
     // add x-axis with ticks
     svg.append("g")
         .attr("transform", `translate(0, ${height - margin})`)
         .attr("id", "x-axis")
-        .call(d3.axisBottom(xScale).ticks(2).tickFormat(d3.format("")));
+        .call(d3.axisBottom(xScale).tickFormat(d3.format("")));
 
     // add y-axis with ticks
     svg.append("g")
         .attr("transform", `translate(${margin},0)`)
         .attr("id", "y-axis")
-        .call(d3.axisLeft(yScale).ticks(12).tickFormat(d3.timeFormat("%M:%S")));
+        .call(d3.axisLeft(yScale).tickFormat(d3.timeFormat("%M:%S")));
 
     // add circle for each data point
     svg.append("g")
         .attr("fill", "steelblue")
-        .selectAll()
+        .selectAll("circle")
         .data(data)
         .join("circle")
         .attr("class", "dot")
