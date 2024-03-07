33d7f05856e49f6056f43b91ab6b68734981501b
Aywin <AywinK@hotmail.com>
2024-03-07 14:18:23 +0000

    4/16 passing

---
+++ script.js
@@ -0,0 +1,61 @@
+
+// state
+let data;
+
+// get data on window load
+window.addEventListener("load", async (e) => {
+    const res = await fetch("https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/cyclist-data.json");
+    const resData = await res.json();
+    if (resData) data = resData;
+    renderChart();
+});
+
+// chart
+function renderChart() {
+    // chart dimensions and margins
+    const width = 800;
+    const height = 500;
+    const margin = 50;
+
+    // x scale
+    const xScale = d3.scaleLinear()
+        .domain([d3.min(data, data => data.Year), d3.max(data, data => data.Year)])
+        .range([margin, width - margin]);
+
+    // y scale
+    const yScale = d3.scaleTime()
+        .domain([d3.min(data, data => new Date(data.Seconds * 1000)), d3.max(data, data => new Date(data.Seconds * 1000))])
+        .range([height - margin, margin]);
+
+    // create svg chart
+    const svg = d3.create("svg")
+        .attr("width", width)
+        .attr("height", height);
+
+    // add x-axis
+    svg.append("g")
+        .attr("transform", `translate(0,${height - margin})`)
+        .attr("id", "x-axis")
+        .call(d3.axisBottom(xScale));
+
+    // add y-axis
+    svg.append("g")
+        .attr("transform", `translate(${margin}, 0)`)
+        .attr("id", "y-axis")
+        .call(d3.axisLeft(yScale));
+
+    // add x-axis with ticks
+    svg.append("g")
+        .attr("transform", `translate(0, ${height - margin})`)
+        .attr("id", "x-axis")
+        .call(d3.axisBottom(xScale).ticks(2));
+
+    // add y-axis with ticks
+    svg.append("g")
+        .attr("transform", `translate(${margin},0)`)
+        .attr("id", "y-axis")
+        .call(d3.axisLeft(yScale).ticks(12).tickFormat(d3.timeFormat("%M:%S")));
+
+    // append svg chart to root
+    document.getElementById("root").append(svg.node());
+}
